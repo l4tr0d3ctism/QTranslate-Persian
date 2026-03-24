16b37f22bf955076c827269e0f895ee3f75f5dca
Ahmed Hatem <ahatem925@gmail.com>
2026-03-24 05:23:00 +0200

    build(api): add maven-publish and versioning
    - enable artifact publishing to maven repositories
    - define groupId and artifactId for the qtranslate-api component
    - set initial version to 1.0.0 for API interface tracking

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,22 @@
+version = "1.0.0"  // only bump when the API interface changes
+
 plugins {
     id("buildsrc.convention.kotlin-jvm")
+    `maven-publish`
 }
 
 dependencies {
     api(libs.kotlinxCoroutines)
     api(libs.bundles.result)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+            groupId    = "com.github.ahatem"
+            artifactId = "qtranslate-api"
+            version    = project.version.toString()
+        }
+    }
 }
